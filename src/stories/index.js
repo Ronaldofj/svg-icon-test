40713fa9d6a0dc208bd55d7c54755af908aac023
Ronaldo <ronaldofigueiredosp@gmail.com>
2020-03-20 10:54:08 -0300

    update storie

--- src/stories/index.js
+++ src/stories/index.js
@@ -20,5 +20,9 @@ export const IconSVG = `
 `;
 
 export const ImportIconSVG = `
-  <IconSVG icon="like" width={100} height={100} fill=""></IconSVG>  
+  <IconSVG icon="like" width={100} height={100} fill=""></IconSVG>
+  <IconSVG icon="like" width={80} height={80} fill=""></IconSVG>
+  <IconSVG icon="like" width={60} height={60} fill=""></IconSVG>
+  <IconSVG icon="like" width={40} height={40} fill=""></IconSVG>
+  <IconSVG icon="like" width={20} height={20} fill=""></IconSVG>  
 `;
